0830d75fb97c9dc5240cdea84ed2a8b0800af06c
Cooper <eliteswimmer42@gmail.com>
2020-12-02 10:26:29 -0600

    updating schema

--- db/employeeOverviewSchema.sql
+++ db/employeeOverviewSchema.sql
@@ -18,3 +18,15 @@ CREATE TABLE roles(
   CONSTRAINT FK_DEPARTMENTID FOREIGN KEY  (department_id)
   REFERENCES departments(id)
 );
+
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
+);
